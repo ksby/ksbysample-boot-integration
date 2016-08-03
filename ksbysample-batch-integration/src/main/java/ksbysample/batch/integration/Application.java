1d3a5b6c32d91038c49d920c129670b8a64d9b12
ksby <kusa@db3.so-net.ne.jp>
2016-08-04 00:56:08 +0900

    #1 実行可能 jar にした時にバッチが終了せずに起動したままになったので、終了させる処理を追加しました

--- ksbysample-batch-integration/src/main/java/ksbysample/batch/integration/Application.java
+++ ksbysample-batch-integration/src/main/java/ksbysample/batch/integration/Application.java
@@ -2,12 +2,14 @@ package ksbysample.batch.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        Runtime.getRuntime().exit(SpringApplication.exit(context));
     }
 
 }
