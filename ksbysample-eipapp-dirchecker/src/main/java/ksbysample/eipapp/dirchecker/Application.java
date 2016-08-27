02243e77a33b2dd0f3b895867db97564b9950652
ksby <kusa@db3.so-net.ne.jp>
2016-08-27 20:46:08 +0900

    #5 Doma 2 を使うためのファイル・設定を追加しました

--- ksbysample-eipapp-dirchecker/src/main/java/ksbysample/eipapp/dirchecker/Application.java
+++ ksbysample-eipapp-dirchecker/src/main/java/ksbysample/eipapp/dirchecker/Application.java
@@ -2,8 +2,10 @@ package ksbysample.eipapp.dirchecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
