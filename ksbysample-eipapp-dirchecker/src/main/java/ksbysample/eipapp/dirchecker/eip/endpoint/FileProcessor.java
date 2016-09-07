6e418491c306a12bc6e8fa7cd125364766b6efb5
ksby <kusa@db3.so-net.ne.jp>
2016-09-07 23:04:30 +0900

    #5 ServiceActivator から UserInfoService#loadUserInfoFromExcel を呼び出すようにしました

--- ksbysample-eipapp-dirchecker/src/main/java/ksbysample/eipapp/dirchecker/eip/endpoint/FileProcessor.java
+++ ksbysample-eipapp-dirchecker/src/main/java/ksbysample/eipapp/dirchecker/eip/endpoint/FileProcessor.java
@@ -1,18 +1,27 @@
 package ksbysample.eipapp.dirchecker.eip.endpoint;
 
+import ksbysample.eipapp.dirchecker.service.userinfo.UserInfoService;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
+import org.xml.sax.SAXException;
 
 import java.io.File;
+import java.io.IOException;
 
 @MessageEndpoint
 public class FileProcessor {
 
+    @Autowired
+    private UserInfoService userInfoService;
+
     @ServiceActivator(inputChannel = "excelToDbChannel")
-    public void process(Message<File> message) throws Exception {
+    public void process(Message<File> message)
+            throws InvalidFormatException, SAXException, IOException {
         File file = message.getPayload();
-        System.out.println(file.getAbsolutePath());
+        userInfoService.loadUserInfoFromExcel(file);
     }
 
 }
