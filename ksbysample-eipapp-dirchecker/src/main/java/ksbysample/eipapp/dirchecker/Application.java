4825eae9ba6c09fc071393d79b3114d9b1c8c957
ksby <kusa@db3.so-net.ne.jp>
2016-09-07 22:51:34 +0900

    #5 applicationContext.xml を用意し Service クラスのメソッド呼び出し時にトランザクションが開始されるように設定しました

--- ksbysample-eipapp-dirchecker/src/main/java/ksbysample/eipapp/dirchecker/Application.java
+++ ksbysample-eipapp-dirchecker/src/main/java/ksbysample/eipapp/dirchecker/Application.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:applicationContext.xml")
 @SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan("ksbysample")
 public class Application {
 
     public static void main(String[] args) {
