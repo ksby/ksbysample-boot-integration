efe1f06021a221026480daf17c5a40ebd7a66b1e
ksby <kusa@db3.so-net.ne.jp>
2019-06-13 22:37:03 +0000

    #24 Spring Initializr でプロジェクトの雛形を作成しました

---
+++ ksbysample-eipapp-aggregator/src/main/java/ksbysample/eipapp/aggregator/KsbysampleEipappAggregatorApplication.java
@@ -0,0 +1,13 @@
+package ksbysample.eipapp.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsbysampleEipappAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsbysampleEipappAggregatorApplication.class, args);
+    }
+
+}
